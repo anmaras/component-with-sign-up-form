3dfc878db8a600149c744587a747f3989a5bbc66
Anthony Maras <marasantonis@gmail.com>
2022-07-23 23:36:42 +0300

    app class update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import TextField from './components/forms/TextField/TextField';
+import Offer from './components/dumb/Offer/Offer';
+import Title from './components/dumb/Title/Title';
 import './App.css';
 
 import React, { Component } from 'react';
@@ -6,22 +8,12 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
-        <section style={{ backgroundColor: 'blue', color: 'white' }}>
-          <h1>Learn to code by watching others</h1>
-          <p>
-            See how experienced developers solve problems in real-time. Watching
-            scripted tutorials is great, but understanding how developers think
-            is invaluable
-          </p>
+      <div className="App">
+        <section className="title-section">
+          <Title />
         </section>
-        <section>
-          <p>
-            <span>Try it free 7 days</span>
-            then $20/mo. thereafter
-          </p>
-        </section>
-        <section>
+        <section className="offer-section">
+          <Offer />
           <TextField />
         </section>
       </div>
