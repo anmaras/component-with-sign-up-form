0cc41fdbeb1854f26ea9dc5e479e66602a96ca45
Anthony Maras <marasantonis@gmail.com>
2022-07-25 12:45:09 +0300

    inline style works for githubpages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import TextField from './components/forms/TextField/TextField';
 import Offer from './components/dumb/Offer/Offer';
 import Title from './components/dumb/Title/Title';
-// import bgMobile from './assets/images/bg-intro-mobile.png';
-// import bgDesktop from './assets/images/bg-intro-desktop.png';
+import bgMobile from './assets/images/bg-intro-mobile.png';
+import bgDesktop from './assets/images/bg-intro-desktop.png';
 import style from './App.module.css';
 
 import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    // const imageUrl = window.innerWidth < 1440 ? bgMobile : bgDesktop;
+    const imageUrl = window.innerWidth < 1440 ? bgMobile : bgDesktop;
     return (
       <div
-        // style={{
-        //   backgroundImage: `url(${imageUrl})`,
-        // }}
+        style={{
+          display: 'grid',
+          backgroundColor: 'rgb(255, 122, 122)',
+          backgroundImage: `url(${imageUrl})`,
+          paddingInline: '1.5rem',
+          minHeight: '100vh',
+        }}
         className={style['main-app']}
       >
         <section className="title-section">
