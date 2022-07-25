180e70276bc95a443a24f7e05c36e7f3a3ae2e5c
Anthony Maras <marasantonis@gmail.com>
2022-07-25 12:25:02 +0300

    disable inline style

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import TextField from './components/forms/TextField/TextField';
 import Offer from './components/dumb/Offer/Offer';
 import Title from './components/dumb/Title/Title';
-import bgMobile from './assets/images/bg-intro-mobile.png';
-import bgDesktop from './assets/images/bg-intro-desktop.png';
+// import bgMobile from './assets/images/bg-intro-mobile.png';
+// import bgDesktop from './assets/images/bg-intro-desktop.png';
 import style from './App.module.css';
 
 import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    const imageUrl = window.innerWidth < 1440 ? bgMobile : bgDesktop;
+    // const imageUrl = window.innerWidth < 1440 ? bgMobile : bgDesktop;
     return (
       <div
+        // style={{
+        //   backgroundImage: `url(${imageUrl})`,
+        // }}
         className={style['main-app']}
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
       >
         <section className="title-section">
           <Title />
